984eea8ab1d98bf9e7067a31621597c54686cc55
Dan J. Hamilton <dhamilton47@cfl.rr.com>
2019-10-15 23:07:00 -0400

    Reset starting point

---
+++ src/components/App/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {BrowserRouter as Router, Route} from "react-router-dom";
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
