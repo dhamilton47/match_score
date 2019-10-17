5612de508e96074db6a4d5434212b4492519f0f6
Dan J. Hamilton <dhamilton47@cfl.rr.com>
2019-10-17 19:25:12 -0400

    Add initial pass at Match Details page
    Including tests

--- src/__tests__/MatchDetails.test.js
+++ src/__tests__/MatchDetails.test.js
@@ -10,6 +10,28 @@ describe('App', () => {
 		const div = document.createElement('div');
 		ReactDOM.render(<MatchDetails />, div);
 		ReactDOM.unmountComponentAtNode(div);
-	});
+    });
+    
+    it('should render the team name', () => {
+        const wrapper = shallow(<MatchDetails/>);
+        expect(wrapper.containsAllMatchingElements([
+            <div>
+                <div>WEST ORANGE WARRIORS BOWLING</div>
+                <form>
+                    Opponent: <input type="text" name="opponent" placeholder="Bishop Moore"></input><br/>
+                    Location: <input type="text" name="location" placeholder="Aloma Bowl"></input><br/>
+                    Home/Away: <input type="text" name="status" placeholder="Home"></input><br/>
+                    Date: <input type="text" name="date" placeholder="Date"></input>
+                </form>
+            </div>,
+            <div className="match_details">	
+                <div>Match Format</div>
+                <form>
+                    Number of Linage Games: <input type="text" name="linage" placeholder="2"></input><br/>
+                    Number of Baker Games: <input type="text" name="baker" placeholder="3"></input>
+                </form>
+            </div>		
+		])).to.equal(true);
+    });
 })
 
--- src/components/content/MatchDetails/MatchDetails.js
+++ src/components/content/MatchDetails/MatchDetails.js
@@ -4,7 +4,26 @@ import './MatchDetails.css';
 class MatchDetails extends Component {
 	render() {
 		return (
-			<div className="match">Match Setup Panel</div>
+			<>
+				<div className="match_details">
+					<div>WEST ORANGE WARRIORS BOWLING</div>
+					
+					<form>
+						Opponent:<input type="text" name="opponent" placeholder="Bishop Moore"></input><br/>
+						Location: <input type="text" name="location" placeholder="Aloma Bowl"></input><br/>
+						Home/Away: <input type="text" name="status" placeholder="Home"></input><br/>
+						Date: <input type="text" name="date" placeholder="Date"></input>
+					</form>
+				</div>
+				<div className="match_details">	
+					<div>Match Format</div>
+
+					<form>
+						Number of Linage Games: <input type="text" name="linage" placeholder="2"></input><br/>
+						Number of Baker Games: <input type="text" name="baker" placeholder="3"></input>
+					</form>
+				</div>
+			</>
 		);
 	}
 }
